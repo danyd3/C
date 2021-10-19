5510ad04b1418283a23e9a8f780b66ef35a120f3
danyd3 <89928594+danyd3@users.noreply.github.com>
2021-10-19 17:20:05 -0500

    Add files via upload
    Hola mundo con C desde Vim

---
+++ helloworld.c
@@ -0,0 +1,15 @@
+/*
+ *Hola mundo c
+ *Title            :Hola mundo en "c" 
+ *Decription       :Hola mundo en "c" con Vim
+ *Author           :Daniel Alberto De Jesus (danyd3)
+ *Date             :20211019
+ *Version          :1
+ */
+
+#include <stdio.h>
+
+int main() {
+	printf("Hola mundo C desde Vim\n");
+		return 0;
+}
